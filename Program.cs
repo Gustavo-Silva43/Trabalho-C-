73af097e708ce09f0144b92a0c13a2ed23ad4850
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2022-10-28 15:10:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = int.Parse(Console.ReadLine());
+
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
