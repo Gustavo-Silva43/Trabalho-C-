fc1d62f1c2c32d27506bd064b0a4e75d3f024979
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2022-10-28 15:21:30 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace Exercicios_while_02
+
+
+
+
+
+using System.Globalization;
+namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom Dia!");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa Noite");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+
+
+
         }
     }
 }
