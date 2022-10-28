d2b81ff3b032c5ab08ebbfd58aa5a2900f70b1b4
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2022-10-28 15:23:55 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Globalization;
 
-namespace course_Parte_3_
+namespace Primeiro
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int idade = 32;
-            double saldo = 10.35784;
-            string nome = "Maria";
-
-            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
-
-
-            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
-
-            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F",CultureInfo.InvariantCulture) + " reais ");
+            Console.WriteLine("Olá Mundo!");
         }
     }
 }
