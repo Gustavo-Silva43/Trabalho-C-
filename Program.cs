07ac97b919e411f8b21df7685ce9bf8d27586113
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2022-10-28 15:22:34 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,22 @@
 ﻿using System;
-
-
-
-
-
-
 using System.Globalization;
-namespace Course
+
+namespace course_Parte_3_
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            char genero = 'F';
             int idade = 32;
             double saldo = 10.35784;
             string nome = "Maria";
 
-            Console.Write("Bom Dia!");
-            Console.WriteLine("Boa Tarde!");
-            Console.WriteLine("Boa Noite");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine(genero);
-            Console.WriteLine(idade);
-            Console.WriteLine(saldo);
-            Console.WriteLine(nome);
-            Console.WriteLine(saldo.ToString("F2"));
-            Console.WriteLine(saldo.ToString("F4"));
-            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
 
 
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
 
-
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F",CultureInfo.InvariantCulture) + " reais ");
         }
     }
 }
